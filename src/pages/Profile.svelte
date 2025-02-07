3c48ea3badff6591a6ae1b02a0a6b75442534bc7
Roi Borromeo <roi.for.school@gmail.com>
2025-02-07 11:23:01 +0800

    Profile get data

--- src/pages/Profile.svelte
+++ src/pages/Profile.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
   import { logout } from "../lib/auth";
   import Button from "../lib/components/ui/button/button.svelte";
+
+  async function getData() {
+    const user = localStorage.getItem("user");
+    const parsedUser = JSON.parse(user);
+    console.table(parsedUser);
+    console.log(parsedUser.username);
+
+    const response = await fetch(
+      `http://localhost:3000/page/user-data${parsedUser.username}`,
+    );
+    const data = await response.json();
+    console.log(data);
+  }
 </script>
 
 <div class="container">
