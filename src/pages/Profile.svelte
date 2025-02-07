2c3028c241b26524fe8dbac130ec828b235d326e
Roi Borromeo <roi.for.school@gmail.com>
2025-02-07 11:51:41 +0800

    Fixed the Profile

--- src/pages/Profile.svelte
+++ src/pages/Profile.svelte
@@ -1,26 +1,51 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { logout } from "../lib/auth";
   import Button from "../lib/components/ui/button/button.svelte";
 
-  async function getData() {
-    const user = localStorage.getItem("user");
-    const parsedUser = JSON.parse(user);
-    console.table(parsedUser);
-    console.log(parsedUser.username);
+  type User = {
+    firstname: string;
+    username: string;
+    email: string;
+    mobile_number: string;
+    created_at: string;
+    updated_at: string;
+  };
 
+  let user_data: User = {
+    firstname: "",
+    username: "",
+    email: "",
+    mobile_number: "",
+    created_at: "",
+    updated_at: "",
+  };
+
+  onMount(() => {
+    getData();
+  });
+
+  async function getData() {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
     const response = await fetch(
-      `http://localhost:3000/page/user-data${parsedUser.username}`,
+      `http://localhost:3000/page/user-data${user.username}`,
     );
     const data = await response.json();
-    console.log(data);
+    user_data = data.user;
+    console.log(user_data);
   }
 </script>
 
 <div class="container">
   <h1>Settings</h1>
   <h1>Nyek</h1>
-  <Button variant="outline">Logout</Button>
   <button on:click={logout}>Logout</button>
+  <button on:click={getData}>Get Data</button>
+  <p>{user_data.username}</p>
+  <p>{user_data.email}</p>
+  <p>{user_data.mobile_number}</p>
+  <p>{user_data.created_at}</p>
+  <p>{user_data.updated_at}</p>
 </div>
 
 <style lang="scss">
